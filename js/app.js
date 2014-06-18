0a9f55e8e9be4d2326602ce2cbcb4504f250a517
johnyi23 <johnyi12125@gmail.com>
2014-06-18 14:49:46 -0700

    commit update to index and app

--- js/app.js
+++ js/app.js
@@ -15,10 +15,41 @@ $(document).ready(function(){
 	$(".menu").hide();
 	showList();
 
+	calc();
+
 });
-var prev = null,  curr = null;
+var itemTotal = 0;
 function showList(){
-	$('.rest').on('click', function(e){
+	$('.rest').on('click', function(){
 		$(this).parent().find('.menu').toggle();
+		itemTotal = 0;
 	});
+}
+
+function calc(){
+	$('input').on('click',function(){
+	var click = $(this);
+	var quantity = click.val();
+	var price = click.data("price");
+	itemTotal = price * quantity;
+
+	click.parent().parent().find('.itemTotal').html("<span class='subTotal'>" + itemTotal + "</span>");
+
+	var orderTotal = 0;
+	
+	for (var i=1; i < 7; i++) {
+		//set var to value inside of last column of row
+		var lineItemCost = parseInt($('.price' + i).find('.subTotal').html());
+		//check to see if its not a valid number, if not set it to 0
+		if(isNaN(lineItemCost) == true) {
+			lineItemCost = 0;
+		}
+		orderTotal += lineItemCost;	
+	}
+		click.parent().parent().parent().find('.orderTotal').html(orderTotal);
+
+	});
+
+
+
 }
