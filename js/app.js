f85a5cdfbc27162e08cc20c41d175458883f200e
brendancwood <brendan@brendancwood.com>
2014-06-17 23:50:22 -0700

    added beginning of local storage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+	// localStorage detection
+	function supportsLocalStorage() {
+	  return typeof(Storage)!== 'undefined';
+	}
+
+	// Run the support check
+	if (!supportsLocalStorage()) {
+	  // No HTML5 localStorage Support
+	} else {
+	  // HTML5 localStorage Support
+
+	}
 	$(".menu").hide();
-	//need to find better way to do this
+	
+	// need to find better way to do this
 	$(".rest1").click(function(){
 		$(".taco").toggle(1000);
 	});
@@ -8,5 +21,13 @@ $(document).ready(function(){
 	$(".rest2").click(function(){
 		$(".burgers").toggle(1000);
 	});
+	// showList();
 
 });
+// function showList(){
+
+// 	$('.rest').on('click', function(){
+// 		console.log(this);
+// 		$(this).toggle(1000);
+// 	});
+// }
